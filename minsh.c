99e101b524df3f248ae2fd5cca75935d7859195f
lauchimoon <laurentino.luna06@gmail.com>
2026-02-08 12:13:45 -0300

    Add PS1 to shell prompt

--- minsh.c
+++ minsh.c
@@ -3,11 +3,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <linux/limits.h>
 #include <signal.h>
 
 #define BUFFER_SIZE 1024
 
+char *username = NULL;
+char *hostname = NULL;
+
 void handle_ctrl_c(int sig);
+void cleanup(void);
+void print_ps1(void);
 char *read_string(void);
 char **split(char *str, int *ntok);
 void execute(char **cmd);
@@ -15,12 +21,16 @@ void execute(char **cmd);
 int main(int argc, char **argv)
 {
     signal(SIGINT, handle_ctrl_c);
+    atexit(cleanup);
+    username = getenv("USER");
+    hostname = malloc(sizeof(char)*PATH_MAX);
+    gethostname(hostname, PATH_MAX);
     while (1) {
+        print_ps1();
         char *prompt = read_string();
         int n_args = 0;
         char **args = split(prompt, &n_args);
         execute(args);
-
         free(args);
         free(prompt);
     }
@@ -29,7 +39,21 @@ int main(int argc, char **argv)
 
 void handle_ctrl_c(int sig)
 {
-    printf("\n");
+    write(STDOUT_FILENO, "\n", 1);
+    print_ps1();
+    fflush(stdout);
+}
+
+void cleanup(void)
+{
+    free(hostname);
+}
+
+void print_ps1(void)
+{
+    char cwd_buffer[PATH_MAX] = { 0 };
+    getcwd(cwd_buffer, PATH_MAX);
+    printf("[%s@%s %s]$ ", username, hostname, cwd_buffer);
 }
 
 char *read_string(void)
@@ -41,8 +65,10 @@ char *read_string(void)
 
     while (1) {
         c = getchar();
-        if (c == EOF)
+        if (c == EOF) {
+            printf("\n");
             exit(0);
+        }
         else if (c == '\n') {
             buffer[pos] = '\0';
             return buffer;
@@ -73,6 +99,9 @@ char **split(char *str, int *ntok)
 
 void execute(char **cmd)
 {
+    if (!cmd[0])
+        return;
+
     pid_t pid = fork();
     if (pid < 0) {
         perror("minsh");
